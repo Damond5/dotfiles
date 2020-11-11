0b324ed4bd2d8faff5c70916d377ab031f9b7da3
Nicky Vølund <nikv5@hotmail.com>
2020-11-11 21:00:43 +0100

    added fix wifi script

---
+++ .fixWifi.sh
@@ -0,0 +1,6 @@
+# wget http://www.killernetworking.com/support/K1535_Debian/board.bin
+
+sudo cp -f ~/board.bin /lib/firmware/ath10k/QCA6174/hw2.1/
+sudo cp -f ~/board.bin /lib/firmware/ath10k/QCA6174/hw3/
+
+# sudo cp -f ~/ath10k.conf /etc/modprobe.d/
