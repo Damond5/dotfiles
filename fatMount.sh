04f7e1a512d9cdaf4e40fbcf8d45490b8916219c
Nicky Vølund <nicky@nordbo-robotics.com>
2021-01-20 08:17:35 +0100

    added more scripts

---
+++ fatMount.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo mount /dev/sda1 ~/usb
---
+++ ping.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import subprocess
+import threading
+
+def ping(ip):
+    # completedProcess = subprocess.run(["ping", "-c", "1", ip, ">", "/dev/null"])
+    completedProcess = subprocess.run(["ping", "-c", "1", ip], capture_output = True)
+    if completedProcess.returncode == 0:
+        print("Found on IP: " + ip)
+
+
+for i in range(256):
+    thread = threading.Thread(target=ping, args=("192.168.0." + str(i),))
+    thread.start()
+
+#     if i == 101:
+#         continue
+#     completedProcess = subprocess.run(["ping", "-c", "1", "192.168.0." + str(i)])
+#     if completedProcess.returncode == 0:
+#         print("Found on IP: 192.168.0." + str(i))
+#         break
