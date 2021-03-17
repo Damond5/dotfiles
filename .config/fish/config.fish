7772778ab8754ac7f76189d591ffc6bec0748279
Nicky Vølund <nikv5@hotmail.com>
2021-03-17 19:24:26 +0100

    changed manpager to vim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,3 +31,7 @@ function sudo
     command sudo $argv
   end
 end
+
+
+# MANPAGER
+set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
