bb098ecc78cf0fff8bca46d08f726448f1b28697
Nicky Vølund <nikv5@hotmail.com>
2022-04-24 12:16:03 +0200

    initial arch prep

--- installElvUI.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp -rf elvui-classic/ElvUI ~/Games/world-of-warcraft-classic/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Interface/AddOns/
-cp -rf elvui-classic/ElvUI_OptionsUI ~/Games/world-of-warcraft-classic/drive_c/Program\ Files\ \(x86\)/World\ of\ Warcraft/_classic_/Interface/AddOns/
