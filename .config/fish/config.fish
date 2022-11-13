a7c7109ded66b9465d4c6a508cc600ac654643c2
Nicky Vølund <nikv5@hotmail.com>
2022-11-13 10:49:29 +0100

    starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,3 +41,6 @@ end
 
 # MANPAGER
 set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
+# PROMPT
+starship init fish | source
