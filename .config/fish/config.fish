bd6d1259b09e0e18e7bf347ad1b152e27648da6c
Nicky <nikv5@hotmail.com>
2024-02-02 00:22:25 +0100

    default manpager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -39,7 +39,7 @@ end
 
 
 # MANPAGER
-set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
 
 
 # PROMPT
