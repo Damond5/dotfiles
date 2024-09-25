3570b1bd3bd0f5ccb412e4f5346b6d1a39faa1f6
nicky <nikv5@hotmail.com>
2024-09-25 19:39:18 +0200

    tv/gamer scripts for any sink id

--- gamer.sh
+++ gamer.sh
@@ -3,4 +3,6 @@
 xrandr --output USB-C-0 --off
 
 xrandr --output DP-0 --mode 3440x1440 --rate 144
-pactl set-default-sink 57
+
+game_sink="$(pactl list sinks short | grep game | awk '{print $1;}')"
+pactl set-default-sink $game_sink
--- tv.sh
+++ tv.sh
@@ -3,4 +3,10 @@
 xrandr --output DP-0 --off
 
 xrandr --output USB-C-0 --mode 1920x1080 --rate 60
-pactl set-default-sink 265
+
+hdmi_sink="$(pactl list sinks short | grep hdmi | awk '{print $1;}')"
+pactl set-default-sink $hdmi_sink
+pactl set-sink-volume $hdmi_sink 0.5
+
+xset s off
+xset -dpms
